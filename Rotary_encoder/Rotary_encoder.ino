97c48008e6bb44aee46dceb201b43de9ad802001
Tamizhanban212 <tamizhanbanag@gmail.com>
2024-02-08 00:14:03 +0530

    Added PID control and designed the shadt connector for 3D print

--- Rotary_encoder/Rotary_encoder.ino
+++ Rotary_encoder/Rotary_encoder.ino
@@ -2,8 +2,8 @@ volatile long temp, counter = 0; //This variable will increase or decrease depen
     
 void setup() {
   Serial.begin (9600);
-  pinMode(2, INPUT_PULLUP); // internal pullup input pin 2 
-  pinMode(3, INPUT_PULLUP); // internalเป็น pullup input pin 3
+  pinMode(4, INPUT_PULLUP); // internal pullup input pin 2 
+  pinMode(2, INPUT_PULLUP); // internal pullup input pin 3
    //Setting up interrupt
   //A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
   attachInterrupt(0, ai0, RISING);
@@ -18,11 +18,11 @@ Serial.println (counter);
 temp = counter;
 }
 }
-  
+
 void ai0() {
 // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
 // Check pin 3 to determine the direction
-if(digitalRead(3)==LOW) {
+if(digitalRead(2)!=LOW) {
 counter++;
 }else{
 counter--;
@@ -32,7 +32,7 @@ counter--;
 void ai1() {
 // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
 // Check with pin 2 to determine the direction
-if(digitalRead(2)==LOW) {
+if(digitalRead(4)!=LOW) {
 counter--;
 }else{
 counter++;
--- motor_driver/motor_driver.ino
+++ motor_driver/motor_driver.ino
@@ -1,15 +1,38 @@
-int a = 8;
-int b = 9;
+/*Example code for testing the Cytron Technologies 10A Rev 2.0 Motor Driver
+ *The code uses only two pins, one for the motor direction (Pin 9) 
+ * and one for the motor speed (pin 9). The speed is set using PWM. 
+ * This code is meant to be run on the Arduino Uno hardware. 
+ */
 
-void setup() 
-{
-pinMode(8, OUTPUT);
-pinMode(9, OUTPUT);
+
+//Pins used to control direction and speed of the motor. Speed pin should be a pwm pin.
+#define MotorDirection 4
+#define MotorSpeed 2
+
+int SpeedVal = 0;
+
+void setup() {
+  //Declaration for the pins used, both should be outputs.
+  pinMode(MotorDirection, OUTPUT);
+  pinMode(MotorSpeed, OUTPUT);
 }
 
-void loop() 
-{
-digitalWrite(a, LOW);
-digitalWrite(b, HIGH);
-
+void loop() {
+  
+  //Ramps up the speed in the clockwise direction.
+  digitalWrite(MotorDirection, LOW);                  //Loop increases the speed slowly until it reaches max speed.
+  for(SpeedVal = 0; SpeedVal < 255; SpeedVal++){
+      analogWrite(MotorSpeed,SpeedVal);
+      delay(40);
+  }
+  analogWrite(MotorSpeed,0);
+  //Ramps up the speed in the counter clockwise direction.  
+  digitalWrite(MotorDirection, HIGH);                //Loop increases the speed slowly until it reaches max speed.
+  for(SpeedVal = 0; SpeedVal < 255; SpeedVal++){
+      analogWrite(MotorSpeed,SpeedVal);
+      delay(40);
+  } 
+  analogWrite(MotorSpeed,0);
+  
 }
+
